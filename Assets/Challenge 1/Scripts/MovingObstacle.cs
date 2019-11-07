75818fdfa29d3e12213e76ea2c560223c1baa9ef
Robin Fros <317370@student.mboutrecht.nl>
2019-11-07 15:16:14 +0100

    no message

---
+++ Assets/Challenge 1/Scripts/MovingObstacle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+
+    public Vector3 startPositie;
+
+    public float begin;
+
+    public float eind;
+
+    public bool omhoog = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPositie = transform.position;
+        begin = startPositie.y;
+        eind = startPositie.y + 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > eind)
+        {
+            omhoog = false;
+        }else if (transform.position.y < begin)
+        {
+            omhoog = true;
+        }
+        if (omhoog)
+        {
+            transform.Translate(x: 0, y: Time.deltaTime * 2f, z: 0);
+        }
+        else
+        {
+            transform.Translate(x: 0, y: Time.deltaTime * -2f, z: 0);
+        }
+    }
+}
---
+++ Assets/Challenge 1/Scripts/MovingObstacle2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle2 : MonoBehaviour
+{
+
+    public Vector3 startPositie;
+
+    public float begin;
+
+    public float eind;
+
+    public bool omlaag = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPositie = transform.position;
+        begin = startPositie.y;
+        eind = startPositie.y + -4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < eind)
+        {
+            omlaag = true;
+        }
+        else if (transform.position.y > begin)
+        {
+            omlaag = false;
+        }
+        if (omlaag)
+        {
+            transform.Translate(x: 0, y: Time.deltaTime * 2f, z: 0);
+        }
+        else
+        {
+            transform.Translate(x: 0, y: Time.deltaTime * -2f, z: 0);
+        }
+    }
+}
